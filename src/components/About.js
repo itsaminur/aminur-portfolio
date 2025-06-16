e0439fc793b1dddfdddacfa08e26cd3719ac1408
sharupc93 <sharupc93@gmail.com>
2025-06-16 15:49:48 +0600

    up

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
   const { t } = useTranslation()
   return (
     <Container sx={{ py: 10, textAlign: 'center' }}>
-      <Typography variant="h3">{t('hero_title')}</Typography>
+      <Typography variant="h3">{t('hero_title')}aaa</Typography>
       <Typography variant="h6" sx={{ mt: 2 }}>{t('hero_subtitle')}</Typography>
       <img src="https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif" alt="gif" style={{ width: '300px', marginTop: 30 }} />
     </Container>
